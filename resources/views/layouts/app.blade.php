7be8394cb1fe9696b5f61fc48385b3a4ea068387
delicha <shoheiyamamoto919@gmail.com>
2023-04-23 00:58:03 +0900

    追加したJSを無効化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Scripts -->
+        @vite(['resources/css/stripe.css', 'resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -33,3 +33,71 @@
         </div>
     </body>
 </html>
+{{-- <script src="https://js.stripe.com/v3/"></script> --}}
+{{-- <script>
+ 
+	// HTMLの読み込み完了後に実行するようにする
+	window.onload = my_init;
+	function my_init() {
+ 
+		// Configに設定したStripeのAPIキーを読み込む  
+		const stripe = Stripe("{{ config('services.stripe.pb_key') }}");
+		const elements = stripe.elements();
+ 
+		var style = {
+			base: {
+			color: "#32325d",
+			fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+			fontSmoothing: "antialiased",
+			fontSize: "16px",
+			"::placeholder": {
+			color: "#aab7c4"
+			}
+		},
+		invalid: {
+			color: "#fa755a",
+			iconColor: "#fa755a"
+		}
+		};
+		
+		const cardElement = elements.create('card', {style: style, hidePostalCode: true});
+		cardElement.mount('#card-element');
+ 
+		const cardHolderName = document.getElementById('card-holder-name');
+		const cardButton = document.getElementById('card-button');
+		const clientSecret = cardButton.dataset.secret;
+ 
+		cardButton.addEventListener('click', async (e) => {
+			// formのsubmitボタンのデフォルト動作を無効にする
+			e.preventDefault();
+			const { setupIntent, error } = await stripe.confirmCardSetup(
+				clientSecret, {
+					payment_method: {
+					card: cardElement,
+					billing_details: { name: cardHolderName.value }
+					}
+				}
+			);
+			
+			if (error) {
+			// エラー処理
+			console.log('error');
+			
+			} else {
+			// 問題なければ、stripePaymentHandlerへ
+			stripePaymentHandler(setupIntent);
+			}
+		});
+	}
+	
+	function stripePaymentHandler(setupIntent) {
+	var form = document.getElementById('payment-form');
+	var hiddenInput = document.createElement('input');
+	hiddenInput.setAttribute('type', 'hidden');
+	hiddenInput.setAttribute('name', 'stripePaymentMethod');
+	hiddenInput.setAttribute('value', setupIntent.payment_method);
+	form.appendChild(hiddenInput);
+	// フォームを送信
+	form.submit();
+	}
+</script> --}} 
